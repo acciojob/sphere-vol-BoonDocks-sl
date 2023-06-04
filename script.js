5094d64d58cc3a79abd8f9b04033ecacbb363544
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-06-04 10:23:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+function volume_sphere(event) {
+  event.preventDefault(); // Prevent form submission and page reload
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+  // Retrieve the input value from the radius field
+  const radiusInput = document.getElementById('radius');
+  const radius = parseFloat(radiusInput.value);
+
+  // Calculate the volume of the sphere using the formula (4/3) * π * r^3
+  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+  // Update the volume field with the calculated volume
+  const volumeInput = document.getElementById('volume');
+  volumeInput.value = volume.toFixed(2);
+}
+
+window.onload = function() {
+  const form = document.getElementById('MyForm');
+  form.addEventListener('submit', volume_sphere);
+};
